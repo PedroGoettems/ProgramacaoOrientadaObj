9e739a4f88f744f0d688d2ac145e7c6aa23f344f
PedroGoettems <pedrootavio.carvalho08@gmail.com>
2026-08-17 15:09:32 -0300

    inicio filtro estudantes

---
+++ src/atividade3/FiltroEstudantes.java
@@ -0,0 +1,15 @@
+package atividade3;
+
+public class FiltroEstudantes {
+    
+    /**
+     * metodo que recebe um array de objetos Estudante, calcula a média de cada um
+     * e retorna um novo array contendo apenas os aprovados (média >= 6.0)
+     * 
+     * @param estudantes array de objetos Estudante
+     * @return array com estudantes aprovados ou null se nenhum foi aprovado
+     */
+    public static Estudante[] filtrarAprovados(Estudante[] estudantes) {
+        return null;
+    }
+}
